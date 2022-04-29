98827609c69c68e20e3483a54fe6346ed5a9d9c7
Jeremiah Chienda <jeremiahchienda@gmail.com>
2022-04-29 15:20:22 +0200

    added form validation

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -13,40 +13,67 @@ function Contact() {
     delay: START_FROM
   });
 
-  const decisionTree = {
+
+  type Input = {
+    EmailInput: string | null
+    TextArea: {
+      text: string
+      required: boolean
+    }
+    Submit: string | null
+  }
+
+  const decisionTree: Record<string, Record<string, Input>> = {
     "Getting Your Resume": {
       "Recruiter": {
         "EmailInput": "Great, Enter Your Company Email",
-        "TextArea": "Feel free to say more (Optional)",
-        "Submit": "Send me Resume"
+        "TextArea": {
+          "text": "Feel free to say more (Optional)",
+          "required": false
+        },
+        "Submit": "Send Updated Resume"
       },
       "Student": {
         "EmailInput": "Awesome, Enter Your Email",
-        "TextArea": "Feel free to say more (Optional)",
-        "Submit": "Send me Resume"
+        "TextArea": {
+          "text": "Feel free to say more (Optional)",
+          "required": false
+        },
+        "Submit": "Send Sample Resume"
       },
       "Professional": {
         "EmailInput": "Great! Enter Your Company Email",
-        "TextArea": "Feel free to say more (Optional)",
-        "Submit": "Send me Resume"
+        "TextArea": {
+          "text": "Feel free to say more (Optional)",
+          "required": false,
+        },
+        "Submit": "Send Sample Resume"
       },
       "Just Stalking": {
         "EmailInput": null,
-        "TextArea": "C'mon Bro...",
+        "TextArea": {
+          "text": "C'mon Bro...",
+          "required": false
+        },
         "Submit": null
       }
     },
-    //["Recruiter", "Student", "Professional", "Just Stalking"],
     "A Speaking Engangement": {
       "School/College": {
         "EmailInput": "Enter Your Email",
-        "TextArea": "Please provide some dates, name of the school and other information if possible",
-        "Submit": "Check your Availability"
+        "TextArea": {
+          "text": "Please provide some dates, name of the school and other information if possible",
+          "required": true
+        },
+        "Submit": "Book Engangement"
       },
       "Conference": {
         "EmailInput": "Enter Your Email",
-        "TextArea": "Please provide some dates, name of Conference, where, and other information if possible",
-        "Submit": "Check your Availability"
+        "TextArea": {
+          "text": "Please provide some dates, name of Conference, where, and other information if possible",
+          "required": true
+        },
+        "Submit": "Book Engangement"
       },
       //"Company": {}
     },
@@ -71,6 +98,8 @@ function Contact() {
 
   const firstSelectRef = useRef(null)
   const secondSelectRef = useRef(null)
+  const emailRef = useRef(null)
+  const messageRef = useRef(null)
 
   type SelectedDecision = {
     firstDecision?: string
@@ -78,16 +107,25 @@ function Contact() {
   }
 
   const [selectedDecisions, setSelectedDecisions] = useState<SelectedDecision>({ firstDecision: null, secondDecision: null })
-
+  const [isFormValid, setIsFormValid] = useState<boolean>(false)
 
   const changeFirstSelect: React.ChangeEventHandler<HTMLSelectElement> = (e) => {
     secondSelectRef.current.value = "default"
     setSelectedDecisions({ firstDecision: e.currentTarget.value })
   }
 
-  function getTargetInput(key: string): string | null {
+
+  function getCurrentInput(): Input | null {
     if (!(selectedDecisions.firstDecision && selectedDecisions.secondDecision)) return null
-    return decisionTree[selectedDecisions.firstDecision][selectedDecisions.secondDecision][key]
+    return decisionTree[selectedDecisions.firstDecision][selectedDecisions.secondDecision]
+  }
+
+  function validateForm() {
+    const emailValue = emailRef?.current?.value ?? "" as String
+    const messageValue = messageRef?.current?.value ?? "" as String
+    const shouldRequireMessage = getCurrentInput()?.TextArea?.required ?? true
+    const isValid = emailValue.length > 5 && (shouldRequireMessage ? messageValue.length > 8 : true)
+    setIsFormValid(isValid)
   }
 
   return (
@@ -105,13 +143,13 @@ function Contact() {
           <option value="default" disabled selected>I'm asking as a...</option>
           {selectedDecisions.firstDecision ? Object.entries(decisionTree[selectedDecisions.firstDecision]).map(entry => <option value={entry[0]}>{entry[0]}</option>) : null}
         </select>
-        <input type="email" required disabled={!getTargetInput("EmailInput")} placeholder={selectedDecisions.secondDecision ? decisionTree[selectedDecisions.firstDecision][selectedDecisions.secondDecision]["EmailInput"] : `I'll be waiting...`} className="w-full max-w-xs input" />
-        <textarea disabled={!getTargetInput("TextArea")} style={{ width: `${firstSelectRef.current?.clientWidth}px` }} className="textarea" placeholder={getTargetInput("TextArea")}></textarea>
+        <input type="email" onChange={_e => validateForm()} ref={emailRef} required disabled={!getCurrentInput()?.EmailInput} placeholder={selectedDecisions.secondDecision ? getCurrentInput()?.EmailInput : `I'll be waiting...`} className="w-full max-w-xs input" />
+        <textarea ref={messageRef} onChange={_e => validateForm()} disabled={!getCurrentInput()?.TextArea?.text} style={{ width: `${firstSelectRef.current?.clientWidth}px` }} className="textarea" placeholder={getCurrentInput()?.TextArea?.text}></textarea>
         <animated.div
           onMouseOver={_e => interactions.startInteraction()}
           onMouseLeave={_e => interactions.endInteraction()}
-          style={button1Styles}><button disabled={!getTargetInput("Submit")} className="btn btn-primary btn-wide md:btn-lg"
-          >{getTargetInput("Submit") ?? "Submit  🤷🏽‍♂️    "}</button></animated.div>
+          style={button1Styles}><button disabled={!isFormValid || !getCurrentInput()?.Submit} className="btn btn-primary btn-wide md:btn-lg"
+          >{getCurrentInput()?.Submit ?? "Submit  🤷🏽‍♂️    "}</button></animated.div>
       </animated.div>
     </div>
   )
